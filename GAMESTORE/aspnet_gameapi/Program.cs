53d82c3ccda5af8f5b7469159ebd1d366a90d4fa
Birasa Divine <b.divine@alustudent.com>
2026-07-29 18:47:36 +0200

    Add database migration extension and integrate it into application startup

--- GAMESTORE/aspnet_gameapi/Program.cs
+++ GAMESTORE/aspnet_gameapi/Program.cs
@@ -6,4 +6,5 @@ var connString = "Data Source=GameStore.db";
 builder.Services.AddSqlite<GameStoreContext>(connString);
 var app = builder.Build();
 app.MapGamesEndpoints();
+app.MigrateDb();
 app.Run();
