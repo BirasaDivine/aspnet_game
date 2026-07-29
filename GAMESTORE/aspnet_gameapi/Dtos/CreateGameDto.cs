7554b7887e5b65980cdf6393f66e519c785ffe55
Birasa Divine <b.divine@alustudent.com>
2026-07-29 16:40:06 +0200

    Add game model and endpoints for CRUD operations, including validation for game creation

--- GAMESTORE/aspnet_gameapi/Dtos/CreateGameDto.cs
+++ GAMESTORE/aspnet_gameapi/Dtos/CreateGameDto.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace aspnet_gameapi.Dtos;
 
 public record CreateGameDto( 
-    string Name, 
+    [Required]string Name, 
     string Genre, 
     decimal Price, 
     DateOnly ReleaseDate);
---
+++ GAMESTORE/aspnet_gameapi/Models/Genre.cs
@@ -0,0 +1,10 @@
+
+namespace aspnet_gameapi.Models;
+
+public class Game
+{
+    public int Id {get; set; }
+    public required string  Name {get; set;}
+}
+    
+    
