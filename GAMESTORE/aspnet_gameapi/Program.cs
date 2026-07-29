62cdec4d3cc41f23deb75592bbf7b1857e173849
Birasa Divine <b.divine@alustudent.com>
2026-07-29 11:37:08 +0200

    Implement initial game listing API with hardcoded GameDto data

---
+++ GAMESTORE/aspnet_gameapi/Program.cs
@@ -0,0 +1,30 @@
+using aspnet_gameapi.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy VII Rebirth",
+        "RPG",
+        69.99M,
+        new DateOnly(2024, 2, 29)),
+    new (
+        3,
+        "Astro Bot",
+        "Platformer",
+        59.99M,
+        new DateOnly(2024, 9, 6)),
+];
+
+//GET /games
+app.MapGet("/games", () => games);
+
+app.Run();
